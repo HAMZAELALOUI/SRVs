73d253d370fe7ffddfb06827c97f7213b29a07d9
Zakaria Lagraini <zakarialaganimes@gmail.com>
2024-04-07 21:55:58 +0000

    add the delete clauses

---
+++ backend/srv/src/main/java/com/project/srv/service/VolService.java
@@ -0,0 +1,44 @@
+package com.project.srv.service;
+
+import com.project.srv.bean.Ville;
+import com.project.srv.bean.Vol;
+import com.project.srv.dao.VolRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.util.List;
+
+@Service
+public class VolService {
+
+
+    public List<Vol> findByDestination(String destination) {
+        return volRepository.findByDestination(destination);
+    }
+
+    public List<Vol> findByHeureDepart(LocalDate heureDepart) {
+        return volRepository.findByHeureDepart(heureDepart);
+    }
+
+    public List<Vol> findByHeureArrivee(LocalDate heureArrivee) {
+        return volRepository.findByHeureArrivee(heureArrivee);
+    }
+
+    public List<Vol> findByPrix(float prix) {
+        return volRepository.findByPrix(prix);
+    }
+
+    public List<Vol> findByVille(Ville ville) {
+        return volRepository.findByVille(ville);
+    }
+
+    public void deleteVolByDestination(String destination) { volRepository.deleteByDestination(destination); }
+    public void deleteVolByHeureDepart(LocalDate heureDepart) { volRepository.deleteByHeureDepart(heureDepart); }
+    public void deleteVolByHeureArrivee(LocalDate heureArrivee) { volRepository.deleteByHeureArrivee(heureArrivee); }
+    public void deleteVolByPrix(float prix) { volRepository.deleteByPrix(prix); }
+    public void deleteAllVols() { volRepository.deleteAll(); }
+
+    @Autowired
+    private VolRepository volRepository;
+}
