a4378c2be225084191296ec8a55db54e22a07ab2
JORD-Reda <84501157+JORD-Reda@users.noreply.github.com>
2024-04-09 16:32:05 +0000

    update 4

--- backend/srv/src/main/java/com/project/srv/ws/ActiviteWs.java
+++ backend/srv/src/main/java/com/project/srv/ws/ActiviteWs.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
-@RestControllergit
+@RestController
 @RequestMapping("/Gestion-Vol/Activite")
 public class ActiviteWs {
 
