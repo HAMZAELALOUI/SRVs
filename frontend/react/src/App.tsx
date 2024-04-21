33c55a7c8bcaac3bda8bccd793cd38531f52e7a6
HAMZAELALOUI <96843983+HAMZAELALOUI@users.noreply.github.com>
2024-04-21 13:28:24 +0100

    admin design

--- frontend/react/src/App.tsx
+++ frontend/react/src/App.tsx
@@ -1,18 +1,20 @@
-import { Routes, Route, useLocation } from "react-router-dom";
+import { Routes, Route, useLocation, Navigate } from "react-router-dom";
 //importing react slick slider
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 import { animateScroll } from "react-scroll";
 
-import NavBar from "./components/organs/NavBar";
 import Home from "./components/pages/Home";
 import { useEffect } from "react";
-import Footer from "./components/organs/Footer";
+
 import SignUp from "./components/organs/SignUp";
 import SignIn from "./components/organs/SignIn";
 import MainLayout from "./components/layouts/MainLayout";
 import UserProfileForm from "./components/pages/UserProfileForm";
+import Dashboard from "./components/pages/Dashboard";
+
+import RegistrationForm from "./components/pages/RegistrationForm";
 
 function App() {
   const directory = useLocation();
@@ -37,6 +39,10 @@ function App() {
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/sign-in" element={<SignIn />} />
         <Route path="/user-profile" element={<UserProfileForm />} />
+
+        {/* ------------------------------ADMIN------------------ */}
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/dashboard/add-user-form" element={<RegistrationForm />} />
       </Routes>
     </div>
   );
